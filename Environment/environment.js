fa72d9a74f3da5f9cb0f560056ebc11653061b6a
davidthakur <vr384695@gmail.com>
2023-12-24 23:27:01 +0530

    changes

---
+++ Environment/environment.js
@@ -0,0 +1,5 @@
+ const environment = {
+    localUrl:"http://localhost:5000/",
+    apiUrl :'https://sociality-app.onrender.com/'
+}
+export default environment;
---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['spng.pngfind.com','https://spng.pngfind.com','i.ibb.co'],
+      },
+}
+
+module.exports = nextConfig

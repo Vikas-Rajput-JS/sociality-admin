df55e22ddaec927be6e13f6adaaff8b6875bdfac
davidthakur <vr384695@gmail.com>
2024-02-19 20:47:20 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['spng.pngfind.com','https://spng.pngfind.com','i.ibb.co'],
+        domains: ['spng.pngfind.com','https://spng.pngfind.com','i.ibb.co',"i.pngimg.me","e7.pngegg.com","reputationprotectiononline.com"],
       },
 }
 

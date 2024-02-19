315277d0a27f4c02f2f43dcd16b2c1f66c28f058
JC25 <vikashrajput@jcsoftwaresolution.com>
2024-02-19 12:49:51 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['spng.pngfind.com','https://spng.pngfind.com','i.ibb.co'],
       },
+      experimental: {
+        nodejs_compat: true,
+      },
 }
 
 module.exports = nextConfig
